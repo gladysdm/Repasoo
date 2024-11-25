b40b21df3a8c8edad5d72d297ff5d11c4f72a8e2
GladisDM <gladysmarr123@gmail.com>
2024-11-25 11:55:09 -0500

    repaso

---
+++ src/main/java/ec/espoch/edu/ec/rep01/Rep01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.espoch.edu.ec.rep01;
+
+/**
+ *
+ * @author USER
+ */
+public class Rep01 {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
